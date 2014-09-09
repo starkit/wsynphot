357b5a1e92f82f9ff6d3b5c847aecc01edb1f3dc
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-09-09 11:57:34 +0200

    various fixes including building in all filters

---
+++ wsynphot/calibration.py
@@ -0,0 +1,20 @@
+import wsynphot
+import os
+from astropy.io import fits
+from astropy import units as u
+from specutils import Spectrum1D
+
+default_vega = 'alpha_lyr_mod_001.fits'
+
+def get_calibration_dir(fname):
+    return os.path.join(wsynphot.__path__[0], 'data', 'calibration', fname)
+
+def get_vega(vega_file=None):
+    if vega_file is None:
+        vega_file = default_vega
+    vega_table = fits.getdata(get_calibration_dir(vega_file), extension=1)
+    vega = Spectrum1D.from_array(vega_table['wavelength'] * u.angstrom,
+                                 vega_table['flux'] *
+                                 u.erg / u.s/ u.cm**2 / u.angstrom)
+
+    return vega
