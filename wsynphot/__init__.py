9ccd45c4d58d1c88b7a59785ee946d8291e65086
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-10-18 18:07:57 -0500

    enabling rtd mode

--- wsynphot/__init__.py
+++ wsynphot/__init__.py
@@ -3,15 +3,17 @@
 """
 This is an Astropy affiliated package.
 """
-
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
+if not on_rtd:
+    from ._astropy_init import *
+    # ----------------------------------------------------------------------------
 
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    from wsynphot.base import BaseFilterCurve, FilterCurve, FilterSet, MagnitudeSet
-    from wsynphot.calibration import get_vega
-    from wsynphot.spectrum1d import SKSpectrum1D as Spectrum1D
+    # For egg_info test builds to pass, put package imports here.
+    if not _ASTROPY_SETUP_:
+        from wsynphot.base import BaseFilterCurve, FilterCurve, FilterSet, MagnitudeSet
+        from wsynphot.calibration import get_vega
+        from wsynphot.spectrum1d import SKSpectrum1D as Spectrum1D
