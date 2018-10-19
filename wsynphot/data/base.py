76126db6de431ded2edded7a97e2f065c33875a2
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-10-19 11:41:59 -0500

    added download to wsynphot

---
+++ wsynphot/data/base.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import os
+import requests
+
+import requests
+from tqdm.autonotebook import tqdm
+
+FILTER_DATA_PATH = os.path.join(os.path.dirname(__file__), 'filter_data.h5')
+
+DATA_TRANSMISSION_URL = ("https://zenodo.org/record/1467244/files/"
+                         "filter_data.h5?download=1")
+
+
+def download_from_url(url, dst):
+    """
+    kindly used from https://gist.github.com/wy193777/0e2a4932e81afc6aa4c8f7a2984f34e2
+    @param: url to download file
+    @param: dst place to put the file
+    """
+    file_size = int(requests.head(url).headers["Content-Length"])
+    if os.path.exists(dst):
+        first_byte = os.path.getsize(dst)
+    else:
+        first_byte = 0
+    if first_byte >= file_size:
+        return file_size
+    header = {"Range": "bytes=%s-%s" % (first_byte, file_size)}
+    pbar = tqdm(
+        total=file_size, initial=first_byte,
+        unit='B', unit_scale=True, desc=url.split('/')[-1])
+    req = requests.get(url, headers=header, stream=True)
+    with(open(dst, 'ab')) as f:
+        for chunk in req.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                pbar.update(1024)
+    pbar.close()
+    return file_size
+
+
+
+
+def download_filter_data():
+    if os.path.exists(FILTER_DATA_PATH):
+        print('Filter Data already exists - you can delete by '
+              'calling wsynphot.delete_filter_data()')
+    download_from_url(DATA_TRANSMISSION_URL, FILTER_DATA_PATH)
+
+def delete_filter_data():
+    if not os.path.exists(FILTER_DATA_PATH):
+        print('Filter Data does not exist - nothing to delete')
+    os.remove(FILTER_DATA_PATH)
