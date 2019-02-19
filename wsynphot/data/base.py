effca87edd169d62a75e483c7bc047d47d932a45
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-02-19 17:29:26 -0500

    add sensible download function

--- wsynphot/data/base.py
+++ wsynphot/data/base.py
@@ -4,6 +4,10 @@ import os
 import requests
 from tqdm.autonotebook import tqdm
 
+
+
+
+
 FILTER_DATA_PATH = os.path.join(os.path.dirname(__file__), 'filter_data.h5')
 
 DATA_TRANSMISSION_URL = ("https://zenodo.org/record/1467309/files/"
