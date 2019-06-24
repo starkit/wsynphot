adfb98f339d6e66fd9dd0c6131083a0b1f2d423d
jaladh-singhal <jaladhsinghal@gmail.com>
2019-06-25 02:37:42 +0530

    Eliminated need of saving file on disk & capitalized constants

--- wsynphot/get_filter_data.py
+++ wsynphot/get_filter_data.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-import requests, tempfile
+import requests
 
 from astropy.io.votable import parse_single_table
 # The VOTables fetched from SVO contain only single table element, thus parse_single_table
 
-svo_main_url = 'http://svo2.cab.inta-csic.es/theory/fps/fps.php'
+SVO_MAIN_URL = 'http://svo2.cab.inta-csic.es/theory/fps/fps.php'
 
 # A utility fuction to convert byte strings to normal literal strings
 def byte_to_literal_strings(dataframe):
@@ -25,13 +25,12 @@ def get_filter_index():
     # To get master list, fetch all filters from SVO of wavelength between 0 & very large no.
     query = {'WavelengthEff_min': 0,
              'WavelengthEff_max': 10000000}
-    response = requests.get(svo_main_url, params=query)
+    response = requests.get(SVO_MAIN_URL, params=query)
     response.raise_for_status()
     
-    # Save the fetched VOTable (XML file) as a temporary file and parse it into an Astropy table
-    with tempfile.NamedTemporaryFile() as votable:
-        votable.write(response.content)
-        table = parse_single_table(votable.name).to_table()
+    # Parse the fetched VOTable (XML file) into an Astropy table
+    votable = io.BytesIO(response.content)
+    table = parse_single_table(votable).to_table()
 
     # TODO: Remove unnecessary columns from table (since there're 31 columns)
     
@@ -44,17 +43,16 @@ def get_transmission_data(filter_id):
     # Fetch transmission data from SVO for the requested filter_id
     # filter_id should be in SVO Filter ID format (facility/instrument.filter)
     query = {'ID': filter_id}
-    response = requests.get(svo_main_url, params=query)
+    response = requests.get(SVO_MAIN_URL, params=query)
     response.raise_for_status()
     
-    # Save the fetched VOTable in a temporary file & try parsing it into an Astropy table
-    with tempfile.NamedTemporaryFile() as votable:
-        votable.write(response.content)
-        try:
-            table = parse_single_table(votable.name).to_table()
-        except ValueError:
-            # If no table element found in VOTable
-            raise ValueError('Requested filter ({0}) does not exist'.format(filter_id))
+    # Try parsing the fetched VOTable into an Astropy table
+    votable = io.BytesIO(response.content)
+    try:
+        table = parse_single_table(votable).to_table()
+    except IndexError:
+        # If no table element found in VOTable
+        raise ValueError('Requested filter ({0}) does not exist'.format(filter_id))
 
     # Return table after converting it to pandas dataframe
     return table.to_pandas()
@@ -64,17 +62,16 @@ def get_filter_list(facility, instrument=None):
     # Fetch all filters for requested facilty and instrument (optional)
     query = {'Facility': facility, 
              'Instrument': instrument}
-    response = requests.get(svo_main_url, params=query)
+    response = requests.get(SVO_MAIN_URL, params=query)
     response.raise_for_status()
     
-    # Save the fetched VOTable in a temporary file & try parsing it into an Astropy table
-    with tempfile.NamedTemporaryFile() as votable:
-        votable.write(response.content)
-        try:
-            table = parse_single_table(votable.name).to_table()
-        except ValueError:
-            # If no table element found in VOTable
-            raise ValueError('No filter found for requested facilty & instrument')
+    # Try parsing the fetched VOTable into an Astropy table
+    votable = io.BytesIO(response.content)
+    try:
+        table = parse_single_table(votable).to_table()
+    except IndexError:
+        # If no table element found in VOTable
+        raise ValueError('No filter found for requested facilty & instrument')
 
     # Return table after converting it to pandas dataframe
     filter_list = table.to_pandas()
