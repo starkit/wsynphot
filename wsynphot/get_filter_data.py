19a77618cef7e293a80155fd040992e8bf0c14de
jaladh-singhal <jaladhsinghal@gmail.com>
2019-06-26 03:37:38 +0530

    Updated functions with docstrings and made them return parsed votable instead of df

--- wsynphot/get_filter_data.py
+++ wsynphot/get_filter_data.py
@@ -1,78 +1,94 @@
 import numpy as np
 import pandas as pd
 import requests
+import io
+from astropy import units as u
 
 from astropy.io.votable import parse_single_table
 # The VOTables fetched from SVO contain only single table element, thus parse_single_table
 
+FLOAT_MAX = np.finfo(np.float64).max
 SVO_MAIN_URL = 'http://svo2.cab.inta-csic.es/theory/fps/fps.php'
 
-# A utility fuction to convert byte strings to normal literal strings
-def byte_to_literal_strings(dataframe):
-    # Select the str columns:
-    str_df = dataframe.select_dtypes([np.object])
-    
-    # Convert all of them into unicode strings
-    str_df = str_df.stack().str.decode('utf-8').unstack()
-    
-    # Swap out converted cols with the original df cols
-    for col in str_df:
-        dataframe[col] = str_df[col]
-    return dataframe
+def get_filter_index(wavelength_eff_min=0, wavelength_eff_max=FLOAT_MAX):
+    """Get master list (index) of all filters at SVO
+    Optional parameters can be given to get filters data for specified
+    Wavelength Eff. range from SVO
 
+    Parameters
+    ----------
+    wavelength_eff_min : float, optional
+        Minimum value of Wavelength Eff. (default is 0)
+    wavelength_eff_max : float, optional
+        Maximum value of Wavelength Eff. (default is a very large no. 
+        FLOAT_MAX - maximum value of np.float64)
 
-def get_filter_index():
-    # To get master list, fetch all filters from SVO of wavelength between 0 & very large no.
-    query = {'WavelengthEff_min': 0,
-             'WavelengthEff_max': 10000000}
+    Returns
+    -------
+    astropy.io.votable.tree.Table object
+        Table element of the VOTable fetched from SVO (in response to query)
+    """
+    wavelength_eff_min = u.Quantity(wavelength_eff_min, u.angstrom)
+    wavelength_eff_max = u.Quantity(wavelength_eff_max, u.angstrom)
+    query = {'WavelengthEff_min': wavelength_eff_min.value,
+             'WavelengthEff_max': wavelength_eff_max.value}    
     response = requests.get(SVO_MAIN_URL, params=query)
     response.raise_for_status()
-    
-    # Parse the fetched VOTable (XML file) into an Astropy table
     votable = io.BytesIO(response.content)
-    table = parse_single_table(votable).to_table()
-
-    # TODO: Remove unnecessary columns from table (since there're 31 columns)
-    
-    # Return table after converting it to pandas dataframe
-    index = table.to_pandas()
-    return byte_to_literal_strings(index)  # Since byte strings appear
+    try:
+        return parse_single_table(votable)
+    except IndexError:
+        # If no table element found in VOTable
+        raise ValueError('No filter found for requested Wavelength Eff. range')
 
 
 def get_transmission_data(filter_id):
-    # Fetch transmission data from SVO for the requested filter_id
-    # filter_id should be in SVO Filter ID format (facility/instrument.filter)
+    """Get transmission data for the requested Filter ID from SVO
+
+    Parameters
+    ----------
+    filter_id : str
+        Filter ID in the format SVO specifies it: 'facilty/instrument.filter'
+
+    Returns
+    -------
+    astropy.io.votable.tree.Table object
+        Table element of the VOTable fetched from SVO (in response to query)
+    """
     query = {'ID': filter_id}
     response = requests.get(SVO_MAIN_URL, params=query)
     response.raise_for_status()
-    
-    # Try parsing the fetched VOTable into an Astropy table
     votable = io.BytesIO(response.content)
     try:
-        table = parse_single_table(votable).to_table()
+        return parse_single_table(votable)
     except IndexError:
         # If no table element found in VOTable
-        raise ValueError('Requested filter ({0}) does not exist'.format(filter_id))
-
-    # Return table after converting it to pandas dataframe
-    return table.to_pandas()
+        raise ValueError('No filter found for requested Filter ID')
 
 
 def get_filter_list(facility, instrument=None):
-    # Fetch all filters for requested facilty and instrument (optional)
+    """Get filters data for requested facilty and instrument from SVO
+
+    Parameters
+    ----------
+    facility : str
+        Facilty for filters
+    instrument : str, optional
+        Instrument for filters (default is None). 
+        Leave empty if there are no instruments for specified facilty
+
+    Returns
+    -------
+    astropy.io.votable.tree.Table object
+        Table element of the VOTable fetched from SVO (in response to query)
+    """
     query = {'Facility': facility, 
              'Instrument': instrument}
     response = requests.get(SVO_MAIN_URL, params=query)
     response.raise_for_status()
-    
-    # Try parsing the fetched VOTable into an Astropy table
     votable = io.BytesIO(response.content)
     try:
-        table = parse_single_table(votable).to_table()
+        return parse_single_table(votable)
     except IndexError:
         # If no table element found in VOTable
-        raise ValueError('No filter found for requested facilty & instrument')
-
-    # Return table after converting it to pandas dataframe
-    filter_list = table.to_pandas()
-    return byte_to_literal_strings(filter_list)  # Since byte strings appear
+        raise ValueError('No filter found for requested Facilty (and Instrument)')
