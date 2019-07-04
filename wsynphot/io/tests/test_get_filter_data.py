7596ce7ec83a5c560e6ce0c8cf1cff6a7e28296b
jaladh-singhal <jaladhsinghal@gmail.com>
2019-07-05 02:00:46 +0530

    Removed class structure from get_filter_data

--- wsynphot/io/tests/test_get_filter_data.py
+++ wsynphot/io/tests/test_get_filter_data.py
@@ -1,15 +1,22 @@
 import pytest
 
-from ..get_filter_data import svofps
+from .. import get_filter_data as gfd
 
 def test_get_filter_index():
-    table = svofps.get_filter_index()
+    table = gfd.get_filter_index()
     # Check if column for Filter ID (named 'filterID') exists in table
     assert 'filterID' in table.to_table().colnames
 
 @pytest.mark.parametrize(('test_filter_id'), 
                         ['HST/NICMOS1.F113N', 'HST/WFPC2.f218w'])
 def test_get_transmission_data(test_filter_id):
-    table = svofps.get_transmission_data(test_filter_id)
+    table = gfd.get_transmission_data(test_filter_id)
     # Check if data is downloaded properly, with > 0 rows
     assert len(table.to_table()) > 0
+
+@pytest.mark.parametrize('test_facility, test_instrument', 
+                            [('HST', 'WFPC2'), ('Keck', None)])
+def test_get_filter_list(test_facility, test_instrument):
+    table = gfd.get_filter_list(test_facility, test_instrument)
+    # Check if column for Filter ID (named 'filterID') exists in table
+    assert 'filterID' in table.to_table().colnames
